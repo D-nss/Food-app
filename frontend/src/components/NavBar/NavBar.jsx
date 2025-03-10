e976eef5fce3a67ecc56e8774bdc596af4ae3fde
daniel <d260404@dac.unicamp.br>
2025-03-10 16:39:35 -0300

    feat: marcar tela atual na barrad de nav

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./NavBar.css"
 import { assets } from '../../assets/assets'
 
 const NavBar = () => {
+
+  const [menu, setMenu] = useState("home")
+
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile app</li>
-        <li>contact us</li>
+        <li className={menu==="home"?"active":""}>home</li>
+        <li className={menu==="menu"?"active":""}>menu</li>
+        <li className={menu==="mobile-app"?"active":""}>mobile app</li>
+        <li className={menu==="contact-us"?"active":""}>contact us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
