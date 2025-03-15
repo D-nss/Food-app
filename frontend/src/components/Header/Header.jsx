65e844e5259c44f1e3106324139f9e16d0d6f088
daniel <d260404@dac.unicamp.br>
2025-03-14 21:16:19 -0300

    fix: making it responsive

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,11 @@ import "./Header.css"
 const Header = () => {
   return (
     <div className='header'>
-      
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa consequuntur amet facilis quis enim dignissimos, ut sunt optio ea harum doloremque reprehenderit voluptas provident exercitationem a iste similique architecto molestiae!</p>
+        <a href='#explore-menu'>View Menu</a>
+      </div>
     </div>
   )
 }
