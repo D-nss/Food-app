5339f7704a78315eacb6be94323516cb97ad2818
unknown <d260404@dac.unicamp.br>
2025-03-20 21:43:50 -0300

    feat: implementação visual da area de login e sign up

--- frontend/src/components/LoginPopUp/LoginPopUp.jsx
+++ frontend/src/components/LoginPopUp/LoginPopUp.jsx
@@ -17,15 +17,15 @@ const LoginPopUp = ({setShowLogin}) => {
             <div className="login-popup-inputs">
                 {currrState==="Login"?<></>:<input type="text" placeholder='Your name' required />}
                 <input type="email" placeholder='Your email' required />
-                <input type="password" placeholder='Pssword' required />
+                <input type="password" placeholder='Password' required />
             </div>
             <button>{currrState ==="Sign Up"?"Create account":"Login"}</button>
-            <div className="login-popup-condition">
-                <input type="checkbox" required/>
-                <p>By continuing, i agree to the terms of use and privacy policy</p>
-            </div>
             {currrState==="Login"?<p>Create a new acount? <span onClick={()=>setCurrStatet("Sign Up")}>Click here</span></p>
-            :<p>Already have an account? <span onClick={()=>setCurrStatet("Login")}>Login here</span ></p>}
+            :<><div className="login-popup-condition">
+            <input type="checkbox" required/>
+            <p>By continuing, i agree to the terms of use and privacy policy</p>
+            </div>
+            <p>Already have an account? <span onClick={()=>setCurrStatet("Login")}>Login here</span ></p></>}
         </form>
         </div>
   )
