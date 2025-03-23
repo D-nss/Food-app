1065dd92332b3264c460513246555deabdda6e09
unknown <d260404@dac.unicamp.br>
2025-03-23 16:41:02 -0300

    feat: infroma quando há compras no carrinho

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,20 +1,24 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import "./NavBar.css"
 import { assets, food_list } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
 const NavBar = ({setShowLogin}) => {
 
   const [menu, setMenu] = useState("home")
 
+  const {getTotalCartAmount} = useContext(StoreContext)
+
   return (
     <div className='navbar'>
       <Link to="/"><img src={assets.logo} alt="" className="logo" /></Link>
       <ul className="navbar-menu">
-        <Link to="/" onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
-        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
-        <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile app</a>
-        <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
+        <Link to="/" onClick={()=>setMenu("home")
+        } className={menu==="home"?"active":""}>home</Link>
+        <a href='/#explore-menu' onClick={()=>{setMenu("menu")}} className={menu==="menu"?"active":""}>menu</a>
+        <a href='/#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile app</a>
+        <a href='/#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
       </ul>
       <div className="navbar-right">
         <div className="navbar-search-icon">
@@ -22,7 +26,7 @@ const NavBar = ({setShowLogin}) => {
         </div>
         <div className="navbar-cart-icon">
           <Link to="/cart"><img src={assets.cart} alt="" /></Link>
-          <div className="dot"></div>
+          <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>Sign in</button>
       </div>
